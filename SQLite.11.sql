08ae4ae033d8c5591841d582b72676e8bc5982a9
muriloresendesouza-byte <murilo.resende.souza@escola.pr.gov.br>
2026-03-17 08:00:21 -0300

    Add files via upload

---
+++ SQLite.11.sql
@@ -0,0 +1,7 @@
+INSERT into produtos(nome,preco, comentario)
+values ('teclado',300,'teclado k300'),
+('monitor',500.1,'monitor drOffice'),
+('mouse',30,'mouse hardline'),
+('webcam',70.90,'webcam logitech'),
+('rtx5090',30.000,'placa de video nvidea');
+SELECT * from produtos;
